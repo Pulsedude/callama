c19ee8862c0af3ae44719978e56b2a3b622dba7f
Coder'Phantom95 <159680854+CoderRony955@users.noreply.github.com>
2026-06-14 22:49:59 +0530

    Added custom Ollama running host

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from default_respones import default_cool_responses
 from router import askmodel
 import random
 
-app = FastAPI(title="callama", description="A API experiement to test or check that how user query sends to AI model and how we got our answers, in which user query or prompt travel through to an API to model, then model return response back to user back.", openapi_url="/openapi.json")
+app = FastAPI(title="callama", description="A API experiment to test or check that how user query sends to AI model and how we got our answers, in which user query or prompt travel through to an API to model, then model return response back to user back.", openapi_url="/openapi.json")
 
 @app.get("/", status_code=status.HTTP_200_OK)
 def default_respones_behaviour():
